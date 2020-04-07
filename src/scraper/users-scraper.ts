ca5fc6fa64a198ce547da3a1a19304b43b71b649
Rubens Mariuzzo <rubens@mariuzzo.com>
2020-04-07 14:58:55 -0400

    Updated users scraper.

--- src/scraper/users-scraper.ts
+++ src/scraper/users-scraper.ts
@@ -5,48 +5,62 @@ import { User } from '../db/models/User'
 
 const userLocations = ['location:"Dominican"']
 
-export const usersScraper = {
-  async scrapeAll(): Promise<void> {
-    const now = new Date()
-    for (const userLocation of userLocations) {
-      await usersScraper.scrape(userLocation, undefined, async (results) => {
-        const users = results.search.nodes.map<Partial<User>>((node) => {
-          return {
-            originalId: node.id,
-            login: node.login,
-            name: node.name,
-            type: node.__typename,
-            url: node.websiteUrl,
-            avatarUrl: node.avatarUrl,
-            company: node.company,
-            location: node.location,
-            createdAt: new Date(node.createdAt),
-            scrapedAt: now,
-            followers: node.followers?.total ?? 0,
-            following: node.following?.total ?? 0,
-          }
-        })
-        console.log({ users })
-        await User.bulkCreate(users)
-      })
-    }
-  },
-  async scrape(
-    query: string,
-    after: string | undefined,
-    fn: (results: SearchUsersQueryResponse) => Promise<void>
-  ): Promise<void> {
-    console.debug(`Scraping users:`, { query, after })
-    const results = await client.request<SearchUsersQueryResponse>(searchUsersQuery, {
-      query,
-      after,
+const searchUsers = async (
+  query: string,
+  after: string | undefined,
+  fn: (results: SearchUsersQueryResponse) => Promise<void>
+): Promise<void> => {
+  console.debug(`Searching users:`, { query, after })
+  const results = await client.request<SearchUsersQueryResponse>(searchUsersQuery, {
+    query,
+    after,
+  })
+
+  await fn(results)
+  logRateLimit(results)
+
+  if (results.search.pageInfo.hasNextPage) {
+    await searchUsers(query, results.search.pageInfo.endCursor, fn)
+  }
+}
+
+export const createOrUpdateUsers = async (users: Partial<User>[]): Promise<void> => {
+  await Promise.all(
+    users.map(async (user) => {
+      const whereQuery = { where: { originalId: user.originalId ?? null } }
+      const existingUser = await User.findOne(whereQuery)
+      if (existingUser) {
+        await User.update(user, whereQuery)
+        console.debug(`Updated user: ${user.login}`)
+      } else {
+        await User.create(user)
+        console.debug(`Created user: ${user.login}`)
+      }
     })
+  )
+}
 
-    await fn(results)
-    logRateLimit(results)
-
-    if (results.search.pageInfo.hasNextPage) {
-      await usersScraper.scrape(query, results.search.pageInfo.endCursor, fn)
-    }
-  },
+export const scrapeUsers = async (): Promise<void> => {
+  const now = new Date()
+  for (const userLocation of userLocations) {
+    await searchUsers(userLocation, undefined, async (results) => {
+      const users = results.search.nodes.map<Partial<User>>((node) => {
+        return {
+          originalId: node.id,
+          login: node.login,
+          name: node.name,
+          type: node.__typename,
+          url: node.websiteUrl,
+          avatarUrl: node.avatarUrl,
+          company: node.company,
+          location: node.location,
+          createdAt: new Date(node.createdAt),
+          scrapedAt: now,
+          followers: node.followers?.total ?? 0,
+          following: node.following?.total ?? 0,
+        }
+      })
+      await createOrUpdateUsers(users)
+    })
+  }
 }
--- src/scripts/scrape.ts
+++ src/scripts/scrape.ts
@@ -1,11 +1,11 @@
 require('dotenv').config()
 import '../db/connection'
 
-import { usersScraper } from '../scraper/users-scraper'
+import { scrapeUsers } from '../scraper/users-scraper'
 
 const scrape = async (): Promise<void> => {
   try {
-    await usersScraper.scrapeAll()
+    await scrapeUsers()
     console.log('Finished scraping users.')
   } catch (error) {
     console.error('Could not scrape users.', error)

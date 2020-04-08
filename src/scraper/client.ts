ac4bb52b44a71b5682e9d2ee8490ba941abb5e07
Rubens Mariuzzo <rubens@mariuzzo.com>
2020-04-07 21:26:20 -0400

    Add OSD_GH_TOKEN and Node v13.12.0.

--- src/scraper/client.ts
+++ src/scraper/client.ts
@@ -2,7 +2,7 @@ import { GraphQLClient } from 'graphql-request'
 
 export const client = new GraphQLClient('https://api.github.com/graphql', {
   headers: {
-    Authorization: `bearer ${process.env.GH_TOKEN}`,
+    Authorization: `bearer ${process.env.OSD_GH_TOKEN}`,
   },
   method: 'POST',
 })

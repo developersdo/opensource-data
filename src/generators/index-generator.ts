514e9946b513941775873287b5429c6eecb61246
Rubens Mariuzzo <rubens@mariuzzo.com>
2020-04-09 21:31:01 -0400

    Add generator for index.

---
+++ src/generators/index-generator.ts
@@ -0,0 +1,21 @@
+import fs from 'fs'
+import path from 'path'
+import ejs from 'ejs'
+
+export const generateIndex = async (): Promise<void> => {
+  return new Promise((resolve) => {
+    const templatePath = path.join(__dirname, 'templates', 'index.ejs')
+    const templateDest = path.join(__dirname, '..', '..', 'docs', 'index.html')
+    const data = {
+      title: 'Opensource Data',
+      githubLink: 'https://github.com/developersdo/opensource-data',
+      dataSources: ['users.json'],
+    }
+
+    ejs.renderFile(templatePath, data, (error, rendered) => {
+      if (error) throw error
+      fs.writeFileSync(templateDest, rendered)
+      resolve()
+    })
+  })
+}
--- src/scripts/generate.ts
+++ src/scripts/generate.ts
@@ -1,10 +1,12 @@
 require('dotenv').config()
 import '../db/connection'
 import { generateUsers } from '../generators/users-generator'
+import { generateIndex } from '../generators/index-generator'
 
 const generate = async (): Promise<void> => {
   try {
     await generateUsers()
+    await generateIndex()
     console.log('Finished generating users.')
   } catch (error) {
     console.error('Could not generate users.', error)
